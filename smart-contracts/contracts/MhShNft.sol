82571197a2c18e6d2a614bb4344efb08e90b2242
kimcodell <kimminhyuk0719@gmail.com>
2022-10-20 00:24:28 +0900

    contract update

--- smart-contracts/contracts/MhShNft.sol
+++ smart-contracts/contracts/MhShNft.sol
@@ -3,18 +3,36 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MhShNft is ERC721URIStorage {
+contract MhShNft is ERC721URIStorage, ERC721Enumerable {
   using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
 
-    function mint(address to, string memory tokenURI) public {
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+      super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+      super._burn(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+      return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+      return super.tokenURI(tokenId);
+    }
+
+    function mint(address to, string memory tokenURI_) public {
         uint256 currentTokenId = _tokenIds.current();
         _mint(to, currentTokenId);
-        _setTokenURI(currentTokenId, tokenURI);
+        _setTokenURI(currentTokenId, tokenURI_);
 
         _tokenIds.increment();
     }
