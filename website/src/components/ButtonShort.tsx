b77453c9b274fc31a6a8dde1ab9e2353b3a4d065
minhyuk02 <minhyuk@indinity.co>
2022-10-21 14:30:33 +0900

    버튼 컴포넌트. ipfs 설치

--- website/src/components/ButtonShort.tsx
+++ website/src/components/ButtonShort.tsx
@@ -1,5 +1,11 @@
-import './ButtonShort.module.css';
+import { ButtonHTMLAttributes, CSSProperties, DetailedHTMLProps } from 'react';
+import styles from './ButtonShort.module.css';
 
-export function ButtonShort() {
-  return <></>
+interface ButtonShortProps {
+  label?: string;
+  buttonStyle?: CSSProperties;
+}
+
+export function ButtonShort({label, buttonStyle, ...props} : ButtonShortProps & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>) {
+  return <button className={styles.short_button} style={buttonStyle} {...props}>{label}</button>
 }
